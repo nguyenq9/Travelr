3788a4a7f5d84b5adec8da7c204dba22ea5440ff
nguyenq9 <nguyenq9@wwu.edu>
2023-01-30 22:47:18 -0800

    making landing page

--- travelpal/src/components/App/App.js
+++ travelpal/src/components/App/App.js
@@ -1,40 +1,27 @@
-import logo from '../../logo.svg';
-import './App.css';
-import ResponsiveAppBar from '../Navigation/ResponsiveAppBar';
-import Footer from '../Footer/Footer';
-import { Route, Routes } from 'react-router-dom';
-import Restaurants from '../Restaurants/Restaurants';
-import Home from '../Home/Home';
-import Guides from '../Guides/Guides';
-import Hotels from '../Hotels/Hotels';
-import Activities from '../Activities/Activities';
+import logo from "../../logo.svg";
+import "./App.css";
+import ResponsiveAppBar from "../Navigation/ResponsiveAppBar";
+import Footer from "../Footer/Footer";
+import { Route, Routes } from "react-router-dom";
+import Restaurants from "../Restaurants/Restaurants";
+import Home from "../Home/Home";
+import Guides from "../Guides/Guides";
+import Hotels from "../Hotels/Hotels";
+import Activities from "../Activities/Activities";
+import Landing from "../Landing/Landing";
 
 function App() {
   return (
     <div className="App">
       <ResponsiveAppBar />
       <Routes>
-          <Route path="Home" element={<Home/>}/>
-          <Route path="Hotels" element={<Hotels/>}/>
-          <Route path="Guides" element={<Guides/>}/>
-          <Route path="Activities" element={<Activities/>}/>
-          <Route path="Restaurants" element={<Restaurants/>}/>
+        <Route path="/" element={<Landing />} />
+        <Route path="Home" element={<Home />} />
+        <Route path="Hotels" element={<Hotels />} />
+        <Route path="Guides" element={<Guides />} />
+        <Route path="Activities" element={<Activities />} />
+        <Route path="Restaurants" element={<Restaurants />} />
       </Routes>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        Thai Nguyen!
-      </header>
       <Footer />
     </div>
   );
---
+++ travelpal/src/components/Landing/Landing.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Landing.css'
+
+class Landing extends React.Component {
+    render(){
+        return (
+            <div className="Landing">
+                <p>Testing</p>
+            </div>
+        );
+    }
+}
+
+export default Landing;
