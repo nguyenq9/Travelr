bee0f9eb08bf2b48170d4e3b267bee0e98378af3
Thuan Nguyen <nguyent59@spu.edu>
2023-02-07 23:39:28 -0800

    Update Again

--- travelr/src/components/List-Activities/List.js
+++ travelr/src/components/List-Activities/List.js
@@ -1,10 +1,28 @@
 import React, {useState} from "react";
 import {Typography, InputLabel, MenuItem, FormControl, Select} from '@mui/material';
-
+import { makeStyles } from '@mui/styles';
 import PikePlace from './images/pike-place.jpg';
 import SpaceNoodle from './images/SpaceNoodle.jpg';
+import { createTheme} from '@mui/material/styles';
 
-import useStyles from './styles';
+
+const theme = createTheme();
+const useStyles = makeStyles({
+  formControl: {
+    margin: theme.spacing(1) , minWidth: 120, marginBottom: '30px', marginTop: '-20px',
+  },
+  container: {
+    padding: '20px',
+  },
+  searchfields: {
+    display: 'flex', justifyContent: 'center', marginBottom: '2.88rem', marginTop: '5px'
+  },
+  searchfieldsInput: {
+    width: '21rem', padding: '.66rem 1rem', marginLeft: '1rem', border: '1px solid #fff',borderRadius: '4px',
+    borderColor: 'inherit', marginTop:'10px', fontSize: '.77rem', fontWeight: '500',
+  },
+  });
+
 
 const List = ()  => {
     const classes = useStyles();
@@ -27,8 +45,8 @@ const List = ()  => {
                 </Select>
             </FormControl>
 
-            <img src={PikePlace} alt="map" style={{width: '90%', heigh: '40%', marginRight:'-50px'}}/>
-            <img src={SpaceNoodle} alt="map" style={{width: '90%', heigh: '40%', marginRight:'-50px'}}/>
+            <img src={PikePlace} alt="map" style={{width: '90%', heigh: '40%', marginRight:'-50px', marginBottom:'10px', borderColor:'blue', borderWidth:'2px'}}/>
+            <img src={SpaceNoodle} alt="map" style={{width: '90%', heigh: '40%', marginRight:'-50px', marginBottom:'10px', borderColor:'blue', borderWidth:'2px'}}/>
       </div>
 
     );
