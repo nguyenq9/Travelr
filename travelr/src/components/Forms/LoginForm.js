e288630c4218ca8c65844b2fadb68e3a0a981963
nguyenq9 <nguyenq9@wwu.edu>
2023-02-21 20:26:15 -0800

    added some auth to the login/signup

--- travelr/src/components/Forms/LoginForm.js
+++ travelr/src/components/Forms/LoginForm.js
@@ -1,14 +1,44 @@
 import React from 'react';
 import './Forms.css';
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as z from "zod";
 
 
-function LoginForm() {
+const formSchema = z.object({
+    email: z.string().email("Enter a valid email address"),
+    password: z.string().min(6, "Password must be at least 6 characters"),
+})
+
+
+
+function LoginForm(props) {
+    const { setUser } = props;
+    const navigate = useNavigate();
+    const { register, handleSubmit, formState: {errors} } =
+    useForm({
+        resolver: zodResolver(formSchema)
+    });
+
+    const loginHandler = (data) => {
+        localStorage.setItem(
+            "user",
+            JSON.stringify({
+                email: data.email,
+            })
+        );
+        setUser({
+            email: data.email
+        })
+        navigate("/", { replace: true });
+    }
+
     return (
         <>
             <div className='bg'>
                 <div className="card">
-                    <form>
+                    <form onSubmit={handleSubmit(loginHandler)}>
                         <h2 className="title"><a href="/" title='Home'>Travelr</a></h2>
                         <p className="or"><span></span></p>
                         <div>
@@ -16,12 +46,17 @@ function LoginForm() {
                             <br></br>
                         </div>
                         <div className="email-login">
+
                             <label htmlFor="email"> <b>Email</b></label>
-                            <input className="email-field" type="text" placeholder="name@abc.com" name="uname" required />
+                            <input className="email-field" type="text" placeholder="name@abc.com" name="uname" {...register("email")} />
+                            {errors.email && <p className="text-danger">{errors.email?.message}</p>}
+
                             <label htmlFor="psw"><b>Password</b></label>
-                            <input type="password" placeholder="8+ (a, A, 1, #)" name="psw" required />
+                            <input type="password" placeholder="8+ (a, A, 1, #)" name="psw" {...register("password")} />
+                            {errors.password && <p className="text-danger">{errors.password?.message}</p>}
+
                         </div>
-                        <button className="cta-btn">Login</button>
+                        <button type="submit" className="cta-btn" >Login</button>
                         <p className="subtitle">Don't have an account? <Link to="/Signup">Sign Up</Link></p>
                     </form>
                 </div>
--- travelr/src/components/Forms/SignupForm.js
+++ travelr/src/components/Forms/SignupForm.js
@@ -1,11 +1,46 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './Forms.css';
+import { Link, useNavigate } from 'react-router-dom'
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as z from "zod";
 
-function SignupForm() {
+const formSchema = z.object({
+    name: z.string().min(1, "Enter your name"),
+    email: z.string().email("Enter a valid email address"),
+    password: z.string().min(6, "Password must be at least 6 characters"),
+    confirmPassword: z.string().min(6, "Confirm password must be at least 6 characters"),
+}).superRefine(({password, confirmPassword}, ctx) => {
+    if (confirmPassword != password) {
+        ctx.addIssue({
+            code: "custom",
+            message: "Password not matched",
+            path: ["confirmPassword"]
+        })
+    }
+})
+
+function SignupForm(props) {
+    const { setUser } = props;
+    const navigate = useNavigate();
+    const { register, handleSubmit, formState: {errors} } = useForm({
+        resolver: zodResolver(formSchema)
+    });
+
+    const signupHandler = (data) => {
+      localStorage.setItem(
+        "user",
+        JSON.stringify({
+          email: data.email,
+        })
+      );
+      setUser({ email: data.email });
+      navigate("/", { replace: true });
+    }
+
     return (
         <div className="card">
-            <form>
+            <form onSubmit={handleSubmit(signupHandler)}>
                 <h2 className="title"><a href="/" title='Home'>Travelr</a></h2>
                 <p className="or"><span></span></p>
 
@@ -15,16 +50,25 @@ function SignupForm() {
                 </div>
 
                 <div className="email-login">
+
                     <label htmlFor="name"> <b>Name</b></label>
-                    <input className="name-field" type="text" placeholder="John Smith" name="uname" required />
+                    <input className="name-field" type="text" placeholder="John Smith" name="uname" {...register("name")} />
+                    {errors.name && <p className="text-danger">{errors.name?.message}</p>}
+                    
                     <label htmlFor="email"> <b>Email</b></label>
-                    <input className="email-field" type="text" placeholder="name@abc.com" name="uname" required />
+                    <input className="email-field" type="text" placeholder="name@abc.com" name="uname" {...register("email")} />
+                    {errors.email && <p className="text-danger">{errors.email?.message}</p>}
+
                     <label htmlFor="psw"><b>Password</b></label>
-                    <input type="password" placeholder="8+ (a, A, 1, #)" name="psw" required />
+                    <input type="password" placeholder="8+ (a, A, 1, #)" name="password" {...register("password")} />
+                    {errors.password && <p className="text-danger">{errors.password?.message}</p>}
+
                     <label htmlFor="psw"><b>Confirm Password</b></label>
-                    <input type="password" placeholder="8+ (a, A, 1, #)" name="psw" required />
+                    <input type="password" placeholder="8+ (a, A, 1, #)" name="confirmPassword" {...register("confirmPassword")}/>
+                    {errors.confirmPassword && <p className="text-danger">{errors.confirmPassword?.message}</p>}
+
                 </div>
-                <button className="cta-btn">Sign up</button>
+                <button type="submit" className="cta-btn">Sign up</button>
 
                 <p className="subtitle">Already a user? <Link to="/login">Login</Link></p>
             </form>
