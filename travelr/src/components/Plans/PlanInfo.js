001384cc42c90ddcc9a93c673a130da5235c8aa6
nguyenq9 <nguyenq9@wwu.edu>
2023-02-27 13:10:34 -0800

    still working on PlanInfo

--- travelr/src/components/Plans/PlanInfo.js
+++ travelr/src/components/Plans/PlanInfo.js
@@ -8,16 +8,20 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import "./PlanInfo.css"
 
+const input_date_style = {
+    width: "100%",
+}
+
 
 function PlanInfo (props) {
     const location = useLocation();
     const { guide_post_id } = useParams();
     const [startDate, setStartDate] = useState(location.state.plan.startDate);
     const [endDate, setEndDate] = useState(location.state.plan.endDate);
-    const [email, setEmail] = useState("name@gmail.com");
-    const [phoneNumber, setPhoneNumber] = useState("+360 555 5555");
-    const [company, setCompany] = useState("Apple Amazon");
-    const [designation, setDesignation] = useState("UI Developer");
+    const [travelers, setTravelers] = useState(location.state.plan.travelers);
+    const [activities, setActivities] = useState(location.state.plan.activities);
+    const [hotels, setHotels] = useState(location.state.plan.hotels);
+    const [restaurants, setRestaurants] = useState(location.state.plan.restaurants);
     const [bio, setBio] = useState(
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
     );
@@ -33,12 +37,14 @@ function PlanInfo (props) {
                             aria-labelledby="account-tab"
                         >
                             <h3 className="myb-3 h3">{location.state.plan.title}</h3>
+                            <h3>{location.state.plan.location}</h3>
                             <div className="cardRow">
                                 <div className="cardRow-md">
                                     <div className="form-gp">
                                         <label>Start Date</label>
                                         <input
                                             type="date"
+                                            style={input_date_style}
                                             className="form-cont"
                                             value={startDate}
                                             onChange={(e) => setStartDate(e.target.value)}
@@ -48,31 +54,39 @@ function PlanInfo (props) {
                                 <div className="cardRow-md">
                                     <div className="form-gp">
                                         <label>End Date</label>
-                                        <input type="date" className="form-cont" value={endDate} onChange={(e) => setEndDate(e.target.value)} />
+                                        <input type="date" className="form-cont" value={endDate} onChange={(e) => setEndDate(e.target.value)} style={input_date_style}/>
                                     </div>
                                 </div>
                                 <div className="cardRow-md">
                                     <div className="form-gp">
-                                        <label>Email</label>
-                                        <input type="text" className="form-cont" value={email} onChange={(e) => setEmail(e.target.value)} />
+                                        <label>Travelers</label>
+                                        {travelers.map(t =>{
+                                            return <input type="text" className="form-cont" value={t} onChange={(e) => setTravelers(e.target.value)} />
+                                        })}
                                     </div>
                                 </div>
                                 <div className="cardRow-md">
                                     <div className="form-gp">
-                                        <label>Phone number</label>
-                                        <input type="text" className="form-cont" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
+                                        <label>Activities</label>
+                                        {activities.map(t =>{
+                                            return <input type="text" className="form-cont" value={t} onChange={(e) => setTravelers(e.target.value)} />
+                                        })}
                                     </div>
                                 </div>
                                 <div className="cardRow-md">
                                     <div className="form-gp">
-                                        <label>Company</label>
-                                        <input type="text" className="form-cont" value={company} onChange={(e) => setCompany(e.target.value)} />
+                                        <label>Hotels</label>
+                                        {hotels.map(t =>{
+                                            return <input type="text" className="form-cont" value={t} onChange={(e) => setTravelers(e.target.value)} />
+                                        })}
                                     </div>
                                 </div>
                                 <div className="cardRow-md">
                                     <div className="form-gp">
-                                        <label>Designation</label>
-                                        <input type="text" className="form-cont" value={designation} onChange={(e) => setDesignation(e.target.value)} />
+                                        <label>Restaurants</label>
+                                        {restaurants.map(t =>{
+                                            return <input type="text" className="form-cont" value={t} onChange={(e) => setTravelers(e.target.value)} />
+                                        })}
                                     </div>
                                 </div>
                             </div>
