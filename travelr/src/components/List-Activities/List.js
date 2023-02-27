5618021e5751877873c8ede1f09d649d14b07058
Thuan Nguyen <nguyent59@spu.edu>
2023-02-27 15:32:22 -0800

    add api autocomplete

--- travelr/src/components/List-Activities/List.js
+++ travelr/src/components/List-Activities/List.js
@@ -1,38 +1,93 @@
-import React, {useState} from "react";
+import {useState} from "react";
+import React from "react";
 import {Typography, InputLabel, MenuItem, FormControl, Select} from '@mui/material';
-import PikePlace from './images/pike-place.jpg';
-import SpaceNoodle from './images/SpaceNoodle.jpg';
-import { createTheme} from '@mui/material/styles';
+//import { createTheme} from '@mui/material/styles';
+import { useRef, useEffect } from "react";
 
-
- const theme = createTheme();
+//  const theme = createTheme();
   const formControl = {
-    marginTop: '20px',minWidth: 100,marginBottom: '30px'
+    minWidth: 100, minHeight:50, marginTop: '20px',marginBottom: '30px', 
+  }
+  const fillBox =  {
+    marginTop: '20px', minWidth: '10vh', marginBottom: '30px',
   }
 
-  const container =  {
-    padding: '20px',
-  }
-  const searchfields = {
-    display: 'flex', justifyContent: 'center', marginBottom: '2.88rem', marginTop: '5px'
-  }
-
-  const searchfieldsInput = {
-    width: '21rem', padding: '.66rem 1rem', marginLeft: '1rem', border: '1px solid #fff',borderRadius: '4px',
-    borderColor: 'inherit', marginTop:'10px', fontSize: '.77rem', fontWeight: '500',
-  }
+  // const searchfields = {
+  //   display: 'flex', justifyContent: 'center', marginBottom: '2.88rem', marginTop: '5px'
+  // }
+  // const searchfieldsInput = {
+  //   width: '21rem', padding: '.66rem 1rem', marginLeft: '1rem', border: '1px solid #fff',borderRadius: '4px',
+  //   borderColor: 'inherit', marginTop:'10px', fontSize: '.77rem', fontWeight: '500',
+  // }
 
 
-const List = ()  => {
+  let autoComplete;
+
+  const loadScript = (url, callback) => {
+    let script = document.createElement("script");
+    script.type = "text/javascript";
+  
+    if (script.readyState) {
+      script.onreadystatechange = function() {
+        if (script.readyState === "loaded" || script.readyState === "complete") {
+          script.onreadystatechange = null;
+          callback();
+        }
+      };
+    } else {
+      script.onload = () => callback();
+    }
+  
+    script.src = url;
+    document.getElementsByTagName("head")[0].appendChild(script);
+  };
+  
+  function handleScriptLoad(updateQuery, autoCompleteRef) {
+    autoComplete = new window.google.maps.places.Autocomplete(
+      autoCompleteRef.current,
+      { types: ["(cities)"], componentRestrictions: { country: "us" } }
+    );
+    autoComplete.setFields(["address_components", "formatted_address"]);
+    autoComplete.addListener("place_changed", () =>
+      handlePlaceSelect(updateQuery)
+    );
+  }
+  
+  async function handlePlaceSelect(updateQuery) {
+    const addressObject = autoComplete.getPlace();
+    const query = addressObject.formatted_address;
+    updateQuery(query);
+    console.log(addressObject);
+  }  
+
+  const List = ()  => {
     const [rating, setRating] = useState('');
+    const [query, setQuery] = useState("");
+    const autoCompleteRef = useRef(null);
 
+    useEffect(() => {
+      loadScript(
+        `https://maps.googleapis.com/maps/api/js?key=AIzaSyCsufdamQaxOQ5M1hsT4x_P5OLe4x7gCN4&libraries=places`,
+        () => handleScriptLoad(setQuery, autoCompleteRef)
+      );
+    }, []);
+    
     return (
-        <div sx = {container}>
-
-            <Typography variant="h4">Attraction Things To Do</Typography>
-            <div sx={searchfields}>
-                <input sx={searchfieldsInput} placeholder="Search City...." />
+        <div>
+            <br></br>
+            <Typography variant="h5">Attraction Things To Do</Typography>
+            <br></br>
+            <Typography variant="h7">Find a Attraction Place Here:</Typography>
+            <div sx={fillBox}>
+              <input
+                ref={autoCompleteRef}
+                onChange={event => setQuery(event.target.value)}
+                placeholder="Enter a City Name"
+                value={query}
+                style={{padding: '10px', marginTop:'5px', borderColor:'black', paddingRight:'20px',}}
+              />
             </div>
+
             <FormControl sx={formControl}>
                 <InputLabel id="rating">Rating</InputLabel>
                 <Select id="rating" value={rating} onChange={(e) => setRating(e.target.value)}>
@@ -43,11 +98,11 @@ const List = ()  => {
                 </Select>
             </FormControl>
 
-            <img src={PikePlace} alt="map" style={{width: '90%', heigh: '40%', marginRight:'-50px', marginBottom:'10px', borderColor:'blue', borderWidth:'2px'}}/>
-            <img src={SpaceNoodle} alt="map" style={{width: '90%', heigh: '40%', marginRight:'-50px', marginBottom:'10px', borderColor:'blue', borderWidth:'2px'}}/>
       </div>
 
     );
 }
 
 export default List;
+
+
