a289ba853ff4562e54722b64a764b7b98c96a21f
Chris <chrisos@pm.me>
2023-03-07 16:39:41 -0800

    Issues v2

--- travelr/src/components/HotelSearch/HotelSearch.js
+++ travelr/src/components/HotelSearch/HotelSearch.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import './HotelSearch.css'
+
+
 function HotelSearch() {
   const [location, setLocation] = useState("");
   const [hotels, setHotels] = useState([]);
@@ -9,9 +11,10 @@ function HotelSearch() {
     searchHotels();
   }, []);
 
+  const API_KEY = process.env.REACT_APP_GO_KEY;
   const searchHotels = async () => {
     const response = await fetch(
-      `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=hotels+in+${location}&fields=price_level&key=AIzaSyDSkmH8ODvpcscHjTv1VVIw9LKqaFgfB1A`
+      `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=hotels+in+${location}&fields=price_level&key=${API_KEY}`
       );
     const data = await response.json();
     const sortedHotels = data.results
@@ -52,14 +55,15 @@ function HotelSearch() {
           <option className="rating">Rating</option>
         </select>
       </div>
-      <div>
       <div className="containerz">
+        <div className="hotel-card">
         {sortedHotels.map((hotel) => (
           <div key={hotel.place_id} onClick={() => handleHotelClick(hotel)}>
             <h2>{hotel.name}</h2>
             <p>Rating: {hotel.rating}</p>
             <p>Reviews: {hotel.user_ratings_total}</p>
           </div>
+          
         ))}
         </div>
       </div>
