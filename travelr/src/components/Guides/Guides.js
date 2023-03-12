f8463efe9c9ca491d8bac8deca303229db3cceab
Ethan Harris <59665405+Ethan-Harris@users.noreply.github.com>
2023-03-12 12:31:56 -0700

    ethan done

--- travelr/src/components/Guides/Guides.js
+++ travelr/src/components/Guides/Guides.js
@@ -1,15 +1,11 @@
 import React, { useEffect, useState } from "react";
 import GuidePreviewBar from "./GuidePreviewBar";
-import italy from "../../images/guides/italy.jpg"
-import route66 from "../../images/guides/route66.jpg"
-import japan from "../../images/guides/japan.jpg"
-
-const lorem = "Synergize productive mindfulness throughput disband the squad but rehydrate as needed paddle on both sides, for shotgun approach. Prepare yourself to swim with the sharks. Lean into that problem dog and pony show, and exposing new ways to evolve our design language, yet hammer out game-plan, or (let's not try to) boil the ocean (here/there/everywhere). Open door policy closing these latest prospects is like putting socks on an octopus, or we have put the apim bol, temporarily so that we can later put the monitors on, or we need to harvest synergy effects, for helicopter view, or optics, exposing new ways to evolve our design language. Flesh that out we need to button up our approach a set of certitudes based on deductions founded on false premise. Net net. Problem territories. Downselect no need to talk to users, just base it on the space calculator, so curate. Dear hiring manager:."
 
 function Guides() {
 
     const [guides, setGuides] = useState([])
 
+    // Fetch all guides and store in a state
     useEffect(() => {
         fetch('/api/getallguides', {
             method: 'POST',
