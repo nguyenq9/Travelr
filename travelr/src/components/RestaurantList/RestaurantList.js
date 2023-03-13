bef05b4082bcdab3d7ab2cf7e77e5ba18181fdda
Ethan Harris <59665405+Ethan-Harris@users.noreply.github.com>
2023-03-12 21:44:04 -0700

    css fix

--- travelr/src/components/RestaurantList/RestaurantList.js
+++ travelr/src/components/RestaurantList/RestaurantList.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './RestaurantList.css';
 import Restaurant from '../Restaurants/Restaurants';
 
+// renders the list of restaurant objects
+// gives the restaurant object the business object and the key, fetched from the Yelp API
 class RestaurantList extends React.Component {
     render() {
         return(
             <div className="RestaurantList">
-                {this.props.businesses.map((business, i) => {  // renders the list of restaurant objects
-                   return <Restaurant business={business} key={business.id} />;  // gives the restaurant object the business object and the key, fetched from the Yelp API
+                {this.props.businesses.map((business, i) => {  
+                   return <Restaurant business={business} key={business.id} />;  
                 })}
             </div>
         )
